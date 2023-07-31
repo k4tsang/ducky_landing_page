eccd5d75ef872660c6fa1c80664afda5dee5667e
Katie Tsang <k4tsang@gmail.com>
2023-07-31 15:19:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
-// Allow users to sign up with email and occupation
+// Function to show the confirmation step
+function showConfirmationStep(email, occupation) {
+  // Hide the form
+  document.getElementById('signup-form').style.display = 'none';
 
-document.getElementById('signup-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+  // Show the confirmation message
+  let confirmationMessage = document.createElement('p');
+  confirmationMessage.textContent = `Thank you for joining the waitlist!
+  We will reach out with our beta shortly.`;
+  document.querySelector('.form-wrapper').appendChild(confirmationMessage);
+}
 
-    let email = document.getElementById('email').value;
-    let occupation = document.getElementById('occupation').value;
+// Function to add the user to the database (you should implement this function for your specific database)
+function addUserToDatabase(email, occupation) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDO2AC2OGSERkZ33iWQVJzzPyK6M6GVp5k",
+    authDomain: "ducky-498d0.firebaseapp.com",
+    projectId: "ducky-498d0",
+    storageBucket: "ducky-498d0.appspot.com",
+    messagingSenderId: "617947101958",
+    appId: "1:617947101958:web:47edf9b3b047b15d853c87",
+    measurementId: "G-ZQCQZTLY3C",
+  };
 
-    let data = {
-        email: email,
-        occupation: occupation
-    };
+  firebase.initializeApp(firebaseConfig);
 
-    fetch('https://your-url.com', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+  // Reference to the "users" collection in Firestore
+  const usersCollection = firebase.firestore().collection('users');
+
+  let data = {
+    email: email,
+    occupation: occupation
+  };
+
+  // Add the data to the "users" collection in Firestore
+  usersCollection.add(data)
+    .then((docRef) => {
+      console.log('Document written with ID:', docRef.id);
+      // Add any success handling here if needed
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
     .catch((error) => {
-        console.error('Error:', error);
+      console.error('Error adding document:', error);
+      // Add error handling here if needed
     });
+}
+
+// Add the event listener outside the function
+document.getElementById('signup-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  let email = document.getElementById('email').value;
+  let occupation = document.getElementById('occupation').value;
+
+  // Show the confirmation step after the form submission
+  showConfirmationStep(email, occupation);
+
+  // Add the user to the database
+  addUserToDatabase(email, occupation);
 });
 
 // Typing animation in header
